2e7314a5e2e7a6c0bd2204996661fd2012014883
linith2244github <linithseang2244@gmail.com>
2026-01-22 22:01:54 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/hello", function(){
+    return "Hello Get";
+});
+
+Route::post("/hello", function(){
+    return "Hello Post";
+});
